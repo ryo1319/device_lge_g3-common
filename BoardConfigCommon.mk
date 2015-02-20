c76afdd4e5ea1308dd5a9a70e6d607e2243458c2
oadam11 <ovadia.adam@gmail.com>
2015-02-20 07:39:05 -0500

    Revert "Update BoardConfig.mk"
    This reverts commit 751376e982163a877e873df20ac79cc29be5c398.
    
    	modified:   BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,10 +57,6 @@ STRICT_ALIASING := true
 # Create changelog
 VALIDUS_CHANGELOG := true
 
-#Block Based OTA Option
-BLOCK_BASED_OTA := false
-
-
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 AUDIO_FEATURE_LOW_LATENCY_PRIMARY := true
